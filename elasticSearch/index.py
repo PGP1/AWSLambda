7d22414fd13f6d8a81327da86749bae3692a199c
rockmanvnx6 <rockmanvnx6@gmail.com>
2020-04-23 21:28:29 +1000

    s3bucket to elastic search lambda function.

---
+++ elasticSearch/index.py
@@ -0,0 +1,39 @@
+import boto3
+import re
+import requests
+import json
+from requests_aws4auth import AWS4Auth
+
+region = 'ap-southeast-2' # e.g. us-west-1
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+host = 'https://search-plantly-es-cheap-my4i72dmshwihajjj2sbwqii3i.ap-southeast-2.es.amazonaws.com' # the Amazon ES domain, including https://
+index = 'lambda-s3-index'
+type = 'lambda-type'
+url = host + '/' + index + '/' + type
+
+headers = { "Content-Type": "application/json" }
+
+s3 = boto3.client('s3')
+
+# Regular expressions used to parse some simple log lines
+ip_pattern = re.compile('(\d+\.\d+\.\d+\.\d+)')
+time_pattern = re.compile('\[(\d+\/\w\w\w\/\d\d\d\d:\d\d:\d\d:\d\d\s-\d\d\d\d)\]')
+message_pattern = re.compile('\"(.+)\"')
+
+# Lambda execution starts here
+def handler(event, context):
+    print("event", event)
+    for record in event['Records']:
+
+        # Get the bucket name and key for the new file
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+
+        # Get, read, and split the file into lines
+        obj = s3.get_object(Bucket=bucket, Key=key)
+        body = obj['Body'].read()
+        
+        r = requests.post(url, auth=awsauth, json=json.loads(body), headers=headers)
