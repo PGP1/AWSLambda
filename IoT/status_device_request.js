72e153e32bef14e23e4ad11741ed005f78514cb8
nugyn <s3429599@student.rmit.edu.au>
2020-04-26 17:42:36 +1000

    push_to_s3.py creates subdirectories, and splits up into individual json files

---
+++ IoT/status_device_request.js
@@ -0,0 +1,24 @@
+console.log('Loading function');
+
+var AWS = require('aws-sdk');
+var iotdata = new AWS.IotData({endpoint: 'a5dhqdjw6qpyv-ats.iot.ap-southeast-2.amazonaws.com'});
+
+exports.handler = async (event, context, callback) => {
+   
+    var params = {
+    topic: 'status_request/b1',
+    payload: JSON.stringify(event),
+    qos: 0
+    };
+    
+    iotdata.publish(params, function(err, data) {
+    if(err){
+    console.log(err);
+    }
+    else{
+    console.log("Success, I guess.");
+    context.succeed();
+    }
+    });
+    callback();
+};
